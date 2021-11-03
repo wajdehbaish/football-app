44fb48b40edeb466e5203d07b6ccb0776854f609
wajdehbaish <wajdehbaish@gmail.com>
2021-11-03 23:21:49 +0200

    done app

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ getVidoes();
   return (
    
     <Router>
-    <div>
+    <div className="mian-main-main">
       <nav>
        <img src='https://images.unsplash.com/photo-1589467785902-054ed88148d8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80'
         alt='picture' width='45' height='45' />
